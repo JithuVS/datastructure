fa5fc0d47a9a97bec1ffb3e88b8dceca29a94169
JithuVS <sreejithsalgunan@gmail.com>
2022-04-08 09:44:11 +0530

    Queue

---
+++ Queue/queue.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(value){
+        var newNode = new Node(value);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        }        
+        else{
+            this.last.next = newNode;
+            this.last = newNode
+        }
+        return this.size++
+    }
+
+    dequeue(){
+        if (!this.first) return null;
+              var temp = this.first; 
+              if (this.first === this.last) {
+                    this.last = null;
+              }
+              this.first = this.first.next;
+              this.size--;
+              return temp;
+    }
+
+}
+
+var Q = new Queue();
+Q.enqueue('A');
+Q.enqueue('B');
+
+Q.enqueue('C');
+Q.enqueue('D');
+
+console.log(Q);
