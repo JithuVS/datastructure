7e72789ceee76a2fe48f3a666df84eda79f6f566
JithuVS <sreejithsalgunan@gmail.com>
2022-04-24 17:12:51 +0530

    binary tree

---
+++ Tree/binary.js
@@ -0,0 +1,93 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    var newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    var current = this.root;
+    while (current.value !== value) {
+      if (current.value < value) {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        } else {
+          current = current.right;
+        }
+      } else {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        } else {
+          current = current.left;
+        }
+      }
+    }
+  }
+
+  find(value) {
+    if (!this.root) return false;
+    var current = this.root;
+    var found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return false;
+    return current;
+  }
+
+  bfs() {
+    let queue = [],
+      data = [];
+    let node = this.root;
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  dfspre() {
+    let data = [];
+
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+}
+
+var tree = new Tree();
+tree.insert(5);
+tree.insert(8);
+tree.insert(4);
+
+tree.find(8);
+
+console.log(tree);
